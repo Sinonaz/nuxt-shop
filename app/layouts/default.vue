7b420ca0679b0109ba3b8dd370b0ebeda60a3420
Isaac <aziksimpson@gmail.com>
2025-08-12 16:33:16 +0200

    Добавляет дефолтный лейаут

---
+++ app/layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <header>Header</header>
+    <main>
+      <slot />
+    </main>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<style scoped lang="less">
+  .app {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  main {
+    flex-grow: 1;
+  }
+</style>
