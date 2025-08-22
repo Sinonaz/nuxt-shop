4a8f0f242b80db2408e06953b01383886091c2e8
Isaac <aziksimpson@gmail.com>
2025-08-22 12:28:31 +0200

    Добавляет компонент селекта

---
+++ app/components/AppSelect.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  const { label, items, name } = defineProps<{
+    label: string
+    name: string
+    items: { label: string; value: string }[]
+  }>()
+
+  const selectValue = defineModel<string>()
+</script>
+
+<template>
+  <div class="select">
+    <select v-model="selectValue" :name>
+      <option value="">{{ label }}</option>
+      <option v-for="item in items" :key="item.value" :value="item.value">{{ item.label }}</option>
+    </select>
+
+    <Icon class="select__icon" name="mdi:arrow-right" size="17px" />
+  </div>
+</template>
+
+<style scoped lang="less">
+  .select {
+    position: relative;
+    max-width: 260px;
+  }
+
+  select {
+    position: relative;
+    appearance: none;
+    width: 100%;
+    background: none;
+    padding: 15px 12px;
+    border-radius: 4px;
+    border: none;
+    box-shadow: inset 0 0 0 1px var(--color-gray);
+    font-family: inherit;
+    font-size: 14px;
+    cursor: pointer;
+  }
+
+  .select__icon {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+  }
+</style>
--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const items = [
+    {
+      label: '1',
+      value: '1'
+    },
+    {
+      label: '2',
+      value: '2'
+    }
+  ]
+  const selectValue = ref<string>('')
+</script>
 
 <template>
-  <h1>catalog</h1>
+  <div>
+    <h1>catalog</h1>
+    <AppSelect v-model="selectValue" :items label="Категория" name="category" />
+  </div>
 </template>
 
 <style scoped lang="less"></style>
