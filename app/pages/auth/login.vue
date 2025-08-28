8ece5418ac33bd66ca0d049188c2fecd1c82546c
Isaac <aziksimpson@gmail.com>
2025-08-28 12:27:35 +0200

    Верстает блок с формой авторизации

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>login</h1>
-</template>
+  <div class="max-w-500px mx-auto">
+    <h1 class="fw-500 text-33px text-center m-0">Мой аккаунт</h1>
 
-<style scoped lang="less"></style>
+    <form class="mt-65px flex flex-col gap-45px">
+      <AppField variant="gray" placeholder="Email" />
+      <AppField variant="gray" placeholder="Пароль" type="password" />
+      <div class="mt-30px flex flex-col gap-15px">
+        <AppButton> Вход </AppButton>
+        <NuxtLink class="mx-auto text-16px hover:text-accent" :to="{ name: 'auth-restore' }">Забыли пароль?</NuxtLink>
+      </div>
+    </form>
+  </div>
+</template>
