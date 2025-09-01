f835cad2050f3a060b9677f59ca99afcfc9d6e85
Isaac <aziksimpson@gmail.com>
2025-09-01 15:59:56 +0200

    Подключает фавикон

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,10 +14,60 @@ export default defineNuxtConfig({
       mode: 'out-in'
     },
     head: {
+      title: 'Магазин SHOPPE',
+      htmlAttrs: {
+        lang: 'ru'
+      },
       link: [
         {
           rel: 'stylesheet',
           href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon/favicon-32x32.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '48x48',
+          href: '/favicon/favicon-48x48.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '72x72',
+          href: '/favicon/favicon-72x72.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '96x96',
+          href: '/favicon/favicon-96x96.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '256x256',
+          href: '/favicon/favicon-256x256.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '384x384',
+          href: '/favicon/favicon-384x384.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '512x512',
+          href: '/favicon/favicon-512x512.png'
+        },
+        {
+          rel: 'manifest',
+          crossorigin: 'anonymous',
+          href: '/favicon/manifest.webmanifest'
         }
       ]
     }
